28ce85b4aee726b8e32289af354e5b91ae08276a
cmhernandezdel <cmhernandezdel@gmail.com>
2019-12-30 13:57:22 +0100

    Now the bot works with more than one argument

--- commands/summoner.js
+++ commands/summoner.js
@@ -3,7 +3,7 @@ const apiKey = require('../keys/riot.json');
 const basePath = "https://euw1.api.riotgames.com";
 const byName= "/lol/summoner/v4/summoners/by-name/";
 
-async function checkELO(accountId){
+async function getSummonerData(accountId){
   const endpoint = "/lol/league/v4/entries/by-summoner/"
   return HttpReq['httpGET'](basePath + endpoint + accountId, apiKey.token)
   .then(res => res)
@@ -12,14 +12,33 @@ async function checkELO(accountId){
 
 module.exports = {
   invoke: async (commandObj) => {
+    var response = "";
+
+    // rank and LP
     if(commandObj['args'].includes("rank")){
-      return HttpReq['httpGET'](basePath + byName + commandObj["params"], apiKey.token)
+      await HttpReq['httpGET'](basePath + byName + commandObj["params"], apiKey.token)
       .then(summonerData => { 
-        return checkELO(summonerData.id)
-        .then(moreSummonerData => (moreSummonerData[0].tier + " " + moreSummonerData[0].rank))
+        return getSummonerData(summonerData.id)
+        .then(moreSummonerData => {
+          response += (moreSummonerData[0].tier + " " + moreSummonerData[0].rank + " " + moreSummonerData[0].leaguePoints + " LP\n")
+        })
         .catch(err => console.log(err))
       })
       .catch(err => console.log(err))
     }
+
+    // wins, losses and winratio
+    if(commandObj['args'].includes("winrate")){
+      await HttpReq['httpGET'](basePath + byName + commandObj["params"], apiKey.token)
+      .then(summonerData => { 
+        return getSummonerData(summonerData.id)
+        .then(moreSummonerData => {
+          response += (moreSummonerData[0].wins + " wins, " + moreSummonerData[0].losses + " losses, " + (Math.round(((moreSummonerData[0].wins / (moreSummonerData[0].losses + moreSummonerData[0].wins)) * 100))) + "% winrate\n")
+        })
+        .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+    }
+    return response;
   }
 };
